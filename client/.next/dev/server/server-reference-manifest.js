e4302580e21a1dd4a0deeeb75b0e32bb8dcd3e98
Ivan Laptev <ilaptev131@gmail.com>
2025-12-12 17:50:39 +0300

    Stop tracking .next

--- client/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VjxxQ5QJXXZNCy6z+e4jsvISpHY/nPixlDbhtuJwP1E=\"\n}"
--- client/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lm+QM7n9x0slZleSO68nt/IyZYur1a/hf5fqmVCFs/U=\"\n}"
